339212a7847e6a6375e610eba4879ed7f5404cca
Whitney Young <wbyoung@fadingred.com>
2014-05-14 18:22:58 -0700

    Use the built-in forEach.
    Arrays actually have a forEach built in to them. This is a little
    different from what we've done thus far, but for now you can just
    think of it as moving word fruits toward the left.

--- each.js
+++ each.js
@@ -1,19 +1,9 @@
-var each = function(array, fn) {
-  var continueIterating = function(array, n, fn) {
-    if (n < array.length) {
-      fn(array[n]);
-      continueIterating(array, n+1, fn);
-    }
-  };
-  continueIterating(array, 0, fn);
-};
-
 var fruits = ['apples', 'oranges', 'bananas'];
 
-each(fruits, function(fruit) {
+fruits.forEach(function(fruit) {
   console.log("I need to purchase " + fruit + ".");
 });
 
-each(fruits, function(fruit) {
+fruits.forEach(function(fruit) {
   console.log("I purchased " + fruit + " today.");
 });
