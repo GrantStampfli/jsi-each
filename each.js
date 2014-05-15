e268e9ab395cbd0fca8c3c24f6b91fb4f0105d1e
Whitney Young <wbyoung@fadingred.com>
2014-05-14 17:50:10 -0700

    Starting point.

---
+++ each.js
@@ -0,0 +1,33 @@
+var indicateFruitThatNeedsPurchasing = function(fruit) {
+  console.log("I need to purchase " + fruit + ".");
+};
+
+var continueIndicatingFruitThatNeedsPurchasing = function(array, n) {
+  if (n < array.length) {
+    indicateFruitThatNeedsPurchasing(array[n]);
+    continueIndicatingFruitThatNeedsPurchasing(array, n+1);
+  }
+};
+
+var startIndicatingFruitThatNeedsPurchasing = function(array) {
+  continueIndicatingFruitThatNeedsPurchasing(array, 0);
+};
+
+var indicateFruitPurchased = function(fruit) {
+  console.log("I purchased " + fruit + " today.");
+};
+
+var continueIndicatingFruitPurchased = function(array, n) {
+  if (n < array.length) {
+    indicateFruitPurchased(array[n]);
+    continueIndicatingFruitPurchased(array, n+1);
+  }
+};
+
+var startIndicatingFruitPurchased = function(array) {
+  continueIndicatingFruitPurchased(array, 0);
+};
+
+var fruits = ['apples', 'oranges', 'bananas'];
+startIndicatingFruitThatNeedsPurchasing(fruits);
+startIndicatingFruitPurchased(fruits);
